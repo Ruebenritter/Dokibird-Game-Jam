2b1aad80e34b7f069c358337e01231a8661cc54d
Leon <54020998+Ruebenritter@users.noreply.github.com>
2025-08-15 15:57:13 +0200

    birds move again

--- src/code_core/prefab_models/base_bird_sprite.gd
+++ src/code_core/prefab_models/base_bird_sprite.gd
@@ -7,7 +7,7 @@ signal screen_visible
 @export var dragoon_type: Enums.dragoon_type = Enums.dragoon_type.Egg
 @export var distance_level: Enums.distance_level = Enums.distance_level.Close
 @export var flightless_bird := false
-@export var bird_skins: Array[Resource]
+#@export var bird_skins: Array[Resource]
 
 # ToDo: add image/sprite animation list to pick from depending on dragoon type
 
@@ -26,93 +26,107 @@ var value: int = 0
 var _skin: BirdSkin
 
 func _ready() -> void:
-    _debounce_hits()
+	scale = Vector2(0.5, 0.5) # Default scale, can be overridden by skin
+	_debounce_hits()
+
+func _process(delta: float) -> void:
+	_move(delta, _goes_left)
 
 func _debounce_hits() -> void:
-    resolve_timer = Timer.new()
-    resolve_timer.wait_time = 0.001 # 1ms
-    resolve_timer.one_shot = true
-    resolve_timer.connect("timeout", Callable(self, "_on_resolve_timeout"))
-    add_child(resolve_timer)
+	resolve_timer = Timer.new()
+	resolve_timer.wait_time = 0.001 # 1ms
+	resolve_timer.one_shot = true
+	resolve_timer.connect("timeout", Callable(self, "_on_resolve_timeout"))
+	add_child(resolve_timer)
 
 func try_construct(type: Enums.dragoon_type, distance: Enums.distance_level, bird_speed: Enums.speed_level) -> bool:
-    value = (distance + type) * bird_speed / 100
-    _apply_skin_for(type)
-    if _skin == null:
-        push_error("No skin found for dragoon type: ", type)
-        return false
-    return true
+	value = (distance + type) * bird_speed / 100
+	z_index = - distance_level + 1000
+	# _apply_skin_for(type)
+	# if _skin == null:
+	#     push_error("No skin found for dragoon type: ", type)
+	#     return false
+	return true
 
 func set_limits(left_x: float, right_x: float, lane_y: float, goes_left: bool) -> void:
-    _map_border_left = left_x
-    _map_border_right = right_x
-    _lane_y = lane_y
-    _goes_left = goes_left
-    global_position.y = _lane_y
+	_map_border_left = left_x
+	_map_border_right = right_x
+	_lane_y = lane_y
+	_goes_left = goes_left
+	global_position.y = _lane_y
 
 func _move(delta: float, goes_left: bool) -> void:
-    if flightless_bird:
-        pass # walk -> only distances allowed near, mid and far for 3 ground lanes
-    else:
-        pass # fly -> all distances allowed for 4 fly lanes
+	if flightless_bird:
+		pass # walk -> only distances allowed near, mid and far for 3 ground lanes
+	else:
+		pass # fly -> all distances allowed for 4 fly lanes
+
+	var dir: int = -1 if goes_left else 1
+	var speed_value: int = speed as int
+	var new_x: float = clamp(global_position.x + (dir * speed_value * delta), _map_border_left, _map_border_right)
+	global_position.x = new_x
+
+	if is_equal_approx(new_x, _map_border_left) or is_equal_approx(new_x, _map_border_right):
+		_goes_left = not goes_left
+		global_position.y = _lane_y + (randf() * 20 - 10) # Randomize Y position slightly when changing direction
 
 
 # Hit handling
 func _start_resolve() -> void:
-    if resolve_timer.is_stopped():
-        resolve_timer.start()
+	if resolve_timer.is_stopped():
+		resolve_timer.start()
 
 func _on_resolve_timeout() -> void:
-    if head_clicked:
-        handle_head_shot()
-    elif body_clicked:
-        handle_body_shot()
+	if head_clicked:
+		handle_head_shot()
+	elif body_clicked:
+		handle_body_shot()
 
 func handle_head_shot() -> void:
-    print("Headshot detected on dragoon of type: ", dragoon_type)
-    shot.emit(self, Enums.hit_zone.Head)
+	print("Headshot detected on dragoon of type: ", dragoon_type)
+	shot.emit(self, Enums.hit_zone.Head)
 
 func handle_body_shot() -> void:
-    print("Body shot detected on dragoon of type: ", dragoon_type)
-    shot.emit(self, Enums.hit_zone.Body)
+	print("Body shot detected on dragoon of type: ", dragoon_type)
+	shot.emit(self, Enums.hit_zone.Body)
 
 func _on_head_area_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-    if event is InputEventMouseButton and event.is_pressed():
-        if event.button_index != MOUSE_BUTTON_LEFT:
-            return
-        head_clicked = true
-        _start_resolve()
+	if event is InputEventMouseButton and event.is_pressed():
+		if event.button_index != MOUSE_BUTTON_LEFT:
+			return
+		head_clicked = true
+		_start_resolve()
 
 func _on_body_area_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-    if event is InputEventMouseButton and event.is_pressed():
-        if event.button_index != MOUSE_BUTTON_LEFT:
-            return
-        body_clicked = true
-        _start_resolve()
+	if event is InputEventMouseButton and event.is_pressed():
+		if event.button_index != MOUSE_BUTTON_LEFT:
+			return
+		body_clicked = true
+		_start_resolve()
 
 
 func _on_visible_on_screen_notifier_2d_screen_entered() -> void:
-    screen_visible.emit(self, true)
+	screen_visible.emit(self, true)
 
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
-    screen_visible.emit(self, false)
-    
+	screen_visible.emit(self, false)
+	
 # visuals
-func _find_skin(e: Enums.dragoon_type) -> BirdSkin:
-    for skin in bird_skins:
-        if skin.type == e:
-            return skin
-    print("No skin found for dragoon type: ", e)
-    return null
+# func _find_skin(e: Enums.dragoon_type) -> BirdSkin:
+#     for skin in bird_skins:
+#         if skin.type == e:
+#             return skin
+#     print("No skin found for dragoon type: ", e)
+#     return null
 
-func _apply_skin_for(e: Enums.dragoon_type) -> void:
-    _skin = _find_skin(e) as BirdSkin
-    if _skin == null:
-        push_error("No skin found for dragoon type: ", e)
-        return
-    
-    self.frames = _skin.frames
-    self.flightless_bird = _skin.is_flightless
-    self.scale = _skin.scale
-    self.animation = "default"
+# func _apply_skin_for(e: Enums.dragoon_type) -> void:
+#     _skin = _find_skin(e) as BirdSkin
+#     if _skin == null:
+#         push_error("No skin found for dragoon type: ", e)
+#         return
+	
+#     self.frames = _skin.frames
+#     self.flightless_bird = _skin.is_flightless
+#     self.scale = _skin.scale
+#     self.animation = "default"
